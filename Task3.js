dcb7befe61587fca8e94e801966f63fc591e0983
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-08 14:35:04 +0200

    3b

--- Task3.js
+++ Task3.js
@@ -1,6 +1,10 @@
-var fruits = ["apple", "strawberry", "banana", "orange","mango"];
+var fruits = ["apple", "strawberry", "banana", "orange", "mango"];
 
- console.log(fruits.every((item, index) => {
-     return typeof(item).toString()=="string";
- }
- ))
+console.log(fruits.every((item, index) => {
+    return typeof (item).toString() == "string";
+}));
+
+console.log(fruits.some((item, index) => {
+    return item.charAt(0) == 'a';
+}));
+
