8b6ea4a325b7866f501a869dbb1a016fc3ebdcec
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-08 17:06:32 +0200

    Task 4 Validation Update

--- Task4.js
+++ Task4.js
@@ -2,29 +2,37 @@ function Info(options= {courseName, courseDuation,courseOwner}){
     if(arguments[1] || (options.courseDuration == null &&
                         options.courseOwner == null &&
                         options.courseName == null) )throw new Error("Invalid Input"); 
-    for(var i in arguments[0].prototype) 
+    
+   
+    for(var i in arguments[0]) 
         {
             var prop = i.toString();
-            if(i!="courseDuration" || i!= "courseOwner" || "courseName") 
+            console.log(prop)
+            if(prop!="courseDuration" && prop!= "courseOwner" && prop!="courseName") 
                {
                    throw new Error("Invalid Input"); 
                }
         }
+    
+    
+    
     let def = {
         courseName: "ES6",
         courseDuration: 4,
         courseOwner: "Eng Mohammed"
     }
     
+    
     let course = Object.assign({},def,options);
+    
     console.log(`Course Name: ${course.courseName}\n Duration: ${course.courseDuration}\n Instructor: ${course.courseOwner}`);
 }
 
-Info({courseName: "React"});
+/*Info({courseName: "React"});
 Info({courseDuration: 5});
 Info({courseOwner: "Eng. Niveen"});
 Info({courseOwner: "Eng. Niveen", courseDuration: 5});
-Info({courseName: "React", courseOwner: "Eng. Niveen", courseDuration: 5});
-Info({courseName: "React", courseOwner: "Eng. Niveen", courseDuration: 5, hello:9});
+Info({courseName: "React", courseOwner: "Eng. Niveen", courseDuration: 5});*/
+Info({courseName: "React", courseOwner: "Eng. Niveen", courseDuration: 5 , extra: 9});
 //Info({courseName: "React", courseOwner: "Eng. Niveen", courseDuration: 5},1);
 //Info(1);
